4f24f0e5e8279ed43f82b14ea8ceb4c38b33865b
Jeanre Swanepoel <jeanre.swanepoel@gmail.com>
2020-11-01 08:19:42 +0200

    add top level TOC

--- src/pages/constitution/Constitution.tsx
+++ src/pages/constitution/Constitution.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonItem, IonList, IonMenu, IonMenuButton, IonMenuToggle, IonPage, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
 import * as Data from '../../assets/data/constitution.json';
 import './Constitution.css';
 
 const Tab1: React.FC = () => {
 
-  const constitutionData = () => {
-    return {
-      __html: Data
-    }
-  }
+  const menu = Data.toc.map((toc, i) => {
+    return (
+      {
+        title: toc.title,
+        id: toc.id,
+        children: toc.children
+      }
+    )
+  });
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Constitution</IonTitle>
+          <IonButtons slot="end">
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
+
+      <IonMenu side="end" menuId="first" contentId="constitution">
+        <IonContent>
+          <IonList>
+            <IonMenuToggle auto-hide="true">
+              {menu.map((item, index) => {
+                return (<IonItem>{item.title}</IonItem>)
+              })}
+              <IonList>
+                <IonItem>
+                  Test Child
+                </IonItem>
+              </IonList>
+            </IonMenuToggle>
+          </IonList>
+        </IonContent>
+      </IonMenu>
+
+      <IonRouterOutlet id="constitution"></IonRouterOutlet>
+
       <IonContent fullscreen>
         <div className="ion-padding">
-          <div className="akoma-ntoso" dangerouslySetInnerHTML={{__html: Data.body}}></div>
+          <div className="akoma-ntoso" dangerouslySetInnerHTML={{ __html: Data.body }}></div>
         </div>
       </IonContent>
     </IonPage >
