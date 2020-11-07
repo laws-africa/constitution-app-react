80e2a0409f44761fdb09b0d2693bc90b8d08dbd7
Greg Kempe <greg@kempe.net>
2020-11-07 10:53:08 +0200

    Use state and ionViewWillEnter to compute view state
    This avoids recomputing expensive operations multiple times whenever
    react re-renders the view component.

--- src/pages/topics/Topic.tsx
+++ src/pages/topics/Topic.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   IonContent,
   IonHeader,
@@ -11,13 +11,14 @@ import {
   IonListHeader,
   IonButtons,
   IonButton,
-  IonIcon
+  IonIcon,
+  useIonViewWillEnter
 } from '@ionic/react';
+import { RouteComponentProps } from 'react-router';
 import { arrowBack } from 'ionicons/icons';
 import data from "../../assets/data/data.json";
 import './Topics.css';
 import parse from 'html-react-parser';
-import { useParams } from "react-router-dom";
 import * as Constitution from '../../assets/data/constitution.json';
 
 function getObject(array: [], key: string, value: string) {
@@ -32,21 +33,38 @@ function getObject(array: [], key: string, value: string) {
   return o;
 }
 
-const Topic: React.FC = () => {
-  let constitutionData: any = (Constitution as any).default;
-  let topic: any = {};
-  let cases = [];
-  let references = [];
-  const { id } = useParams();
-  topic = data.topics.find((topic) => topic.id === id);
-  for (const reference of topic.references) {
-    const linkedReference = getObject(constitutionData.toc, "id", reference);
-    references.push(linkedReference);
-  }
-  for (const caseId of topic.cases) {
-    const linkedCase = data.cases.find((c) => c.id === caseId);
-    cases.push(linkedCase);
-  }
+interface TopicProps extends RouteComponentProps<{ id: string; }> { }
+
+const Topic: React.FC<TopicProps> = ({ match }) => {
+  const [topic, setTopic] = useState({title: '', content: ''});
+  const [cases, setCases] = useState([]);
+  const [references, setReferences] = useState([]);
+
+  useIonViewWillEnter(() => {
+    const topic = data.topics.find(t => t.id === match.params.id);
+    // @ts-ignore
+    setTopic(topic);
+
+    let constitutionData: any = (Constitution as any).default;
+    let cases = [];
+    let references = [];
+
+    if (topic) {
+      for (const reference of topic.references) {
+        const linkedReference = getObject(constitutionData.toc, "id", reference);
+        references.push(linkedReference);
+      }
+      for (const caseId of topic.cases) {
+        const linkedCase = data.cases.find((c) => c.id === caseId);
+        cases.push(linkedCase);
+      }
+    }
+
+    // @ts-ignore
+    setCases(cases);
+    // @ts-ignore
+    setReferences(references);
+  });
 
   const previous = () => {
     window.history.back()
