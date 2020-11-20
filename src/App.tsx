dc673a0d573d8a4e1edc95af8994d0ce011cd78d
Greg Kempe <greg@kempe.net>
2020-11-20 12:40:23 +0200

    Tolerat / in route ids

--- src/App.tsx
+++ src/App.tsx
@@ -51,8 +51,8 @@ const App: React.FC = () => (
           <Route path="/guides/:id" component={Topic} exact={true} />
           <Route path="/constitution/toc" component={ConstitutionTOC} exact={true} />
           <Route path="/constitution/full" component={Constitution_Full} exact={true} />
-          <Route path="/constitution/full/:id" component={Constitution_Full} exact={true} />
-          <Route path="/constitution/provision/:id" component={Constitution} exact={true} />
+          <Route path="/constitution/full/:id+" component={Constitution_Full} exact={true} />
+          <Route path="/constitution/provision/:id+" component={Constitution} exact={true} />
           <Route path="/home" component={Home} exact={true} />
           <Route path="/cases" component={Cases} exact={true} />
           <Route path="/cases/:id" component={Case} exact={true} />
