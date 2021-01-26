be5207b6e660a4df39514ef1cf2f213d605fd80e
Greg Kempe <greg@kempe.net>
2021-01-26 10:41:36 +0200

    Setup service workers and make android homescreen install work

--- src/index.tsx
+++ src/index.tsx
@@ -15,9 +15,7 @@ if (path.length) {
   setRedirect(path);
 }
 
-window.history.pushState({}, '', '/cases');
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
