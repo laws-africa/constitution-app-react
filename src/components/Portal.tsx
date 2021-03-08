40b950b0113fb49a1a57f9a85de4dd70a6649955
Greg Kempe <greg@kempe.net>
2021-03-08 15:37:25 +0200

    Inject decorations

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -2,35 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 interface PortalProps { 
-  id: string;
-  constitution: any;
+  element: HTMLElement;
 }
 
 class Portal extends React.Component<PortalProps> {
-  private readonly provision: any;
-  private readonly el: HTMLElement;
-  private readonly constitution: Document;
-
-  constructor(props: any) {
-    super(props);
-    this.el = document.createElement('div');
-    this.constitution = this.props.constitution;
-    this.provision = this.constitution.getElementById(this.props.id);
-  }
-
-  componentDidMount() {
-    const heading = this.provision.firstChild;
-    heading.parentNode.insertBefore(this.el, heading.nextSibling);
-  }
-
-  componentWillUnmount() {
-    this.provision.removeChild(this.el);
-  }
-
   render() {
     return ReactDOM.createPortal(
       this.props.children,
-      this.el
+      this.props.element,
     );
   }
 }
