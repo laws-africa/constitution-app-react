c97cbc6779a95b3c2920d4c0731a8588443aeb9f
Sandravaphilips <lacasandra1@gmail.com>
2021-03-24 04:38:00 -0700

    exported the icons

---
+++ src/assets/svgs/index.js
@@ -0,0 +1,17 @@
+import GUIDES from "./icons/Guides.svg";
+import RULES from "./icons/Rules_of_the_Assembly.svg";
+import HOME from "./icons/home.svg";
+import SEARCH from "./icons/Search.svg";
+import CASES from "./icons/Cases.svg";
+import CONSTITUTION from "./icons/Constitution.svg";
+import HELP from "./icons/Help.svg";
+
+export const svgs = {
+  GUIDES,
+  RULES,
+  HELP,
+  HOME,
+  SEARCH,
+  CASES,
+  CONSTITUTION,
+}
