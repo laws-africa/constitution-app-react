d543e35d8dc0aea5604d7bd9cf5628ea3c3c8432
Sandravaphilips <lacasandra1@gmail.com>
2021-03-25 13:30:07 -0700

    styled each topic item as a card

--- src/components/topic.tsx
+++ src/components/topic.tsx
@@ -1,4 +1,4 @@
-import {IonItem, IonLabel, IonThumbnail} from "@ionic/react";
+import {IonCard, IonCardContent, IonCol, IonIcon} from "@ionic/react";
 import parse from "html-react-parser";
 import React from "react";
 
@@ -8,16 +8,26 @@ interface TopicItemProps {
 
 export const TopicItem: React.FC<TopicItemProps> = ({ topic }) => {
   return (
-    <IonItem key={"topic-" + topic.id} routerLink={"/guides/" + topic.id}>
-      <IonThumbnail slot="start">
-        <img src={"../../assets/images/" + topic.id + ".svg"} onError={(e) => {
-          e.currentTarget.src = "../../assets/shapes.svg"
-        }} alt={topic.title}/>
-      </IonThumbnail>
-      <IonLabel>
-        <h3 className="ion-text-wrap">{topic.title}</h3>
-        <p className="ion-text-wrap">{parse(topic.snippet)}</p>
-      </IonLabel>
-    </IonItem>
+    <IonCard
+      class="topic-item"
+      key={"topic-" + topic.id}
+      routerLink={"/guides/" + topic.id}
+    >
+      <div>
+        <IonCol>
+          <IonIcon
+            size="large"
+            icon={"../../assets/images/" + topic.id + ".svg"}
+            onError={(e) => {
+              e.currentTarget.src = "../../assets/shapes.svg";
+            }}
+          ></IonIcon>
+        </IonCol>
+        <IonCardContent>
+          <h3 className="ion-text-wrap">{topic.title}</h3>
+          <p className="ion-text-wrap">{parse(topic.snippet)}</p>
+        </IonCardContent>
+      </div>
+    </IonCard>
   );
 };
