ed8b84b48c7aff03d5ca92879efb7ef03c0e1a15
Sandravaphilips <lacasandra1@gmail.com>
2021-03-25 13:33:08 -0700

    styled the guides tab according to the design

--- src/pages/topics/Topics.tsx
+++ src/pages/topics/Topics.tsx
@@ -1,37 +1,49 @@
-import React from 'react';
+import React from "react";
 import {
   IonContent,
   IonHeader,
   IonPage,
-  IonTitle,
-  IonToolbar,
   IonText,
   IonList,
-} from '@ionic/react';
+  IonButton,
+  IonCol,
+  IonIcon,
+} from "@ionic/react";
 import data from "../../assets/data/data.json";
-import './Topics.css';
-import { TopicItem } from '../../components/topic';
+import "./Topics.css";
+import { TopicItem } from "../../components/topic";
+import { svgs } from "../../assets/svgs";
 
 const Topics: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle class="ion-title">
-            Guides
-          </IonTitle>
-        </IonToolbar>
+      <IonHeader class="ion-padding-horizontal ion-padding-top">
+        <section className="tab-title">
+          <IonCol size="1" class="icon ion-no-padding">
+            <IonIcon size="small" icon={svgs.GUIDES}></IonIcon>
+          </IonCol>
+          <h2>Guides</h2>
+          <IonButton
+            className="ion-no-padding"
+            fill="clear"
+            routerLink={"/search/guides"}
+          >
+            Search
+          </IonButton>
+        </section>
+        <hr className="header-divider" />
       </IonHeader>
-        <IonContent>
-          <IonText>
-            <div className="ion-padding">
-              Guides to the sections the Constitution.
-            </div>
-          </IonText>
-          <IonList>
-            {data.topics.map((topic) => (
-              <TopicItem topic={topic} />
-            ))}
+      <IonContent>
+        <IonText>
+          <div className="ion-padding">
+            Guides to the sections the Constitution.
+            <hr className="list-divider" />
+          </div>
+        </IonText>
+        <IonList>
+          {data.topics.map((topic) => (
+            <TopicItem topic={topic} />
+          ))}
         </IonList>
       </IonContent>
     </IonPage>
