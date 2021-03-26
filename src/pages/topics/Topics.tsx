7ef95c1e601b701c69e30d4b71be3754f26e79ce
Sandravaphilips <lacasandra1@gmail.com>
2021-03-26 04:43:13 -0700

    restructured the Topics using ConstitutionTOC as a reference

--- src/pages/topics/Topics.tsx
+++ src/pages/topics/Topics.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import {
   IonContent,
-  IonHeader,
   IonPage,
-  IonText,
   IonList,
   IonButton,
   IonCol,
@@ -17,29 +15,27 @@ import { svgs } from "../../assets/svgs";
 const Topics: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader class="ion-padding-horizontal ion-padding-top">
-        <section className="tab-title">
-          <IonCol size="1" class="icon ion-no-padding">
-            <IonIcon size="small" icon={svgs.GUIDES}></IonIcon>
-          </IonCol>
-          <h2>Guides</h2>
-          <IonButton
-            className="ion-no-padding"
-            fill="clear"
-            routerLink={"/search/guides"}
-          >
-            Search
-          </IonButton>
-        </section>
-        <hr className="header-divider" />
-      </IonHeader>
       <IonContent>
-        <IonText>
-          <div className="ion-padding">
-            Guides to the sections the Constitution.
-            <hr className="list-divider" />
-          </div>
-        </IonText>
+        <div className="ion-padding">
+          <section className="tab-title">
+            <IonCol size="1" class="icon ion-no-padding">
+              <IonIcon size="small" icon={svgs.GUIDES}></IonIcon>
+            </IonCol>
+            <h2>Guides</h2>
+            <IonButton
+              className="ion-no-padding"
+              fill="clear"
+              routerLink={"/search/guides"}
+            >
+              Search
+            </IonButton>
+          </section>
+
+          <hr className="header-divider" />
+
+          <p>Guides to the sections the Constitution.</p>
+          <hr className="list-divider" />
+        </div>
         <IonList>
           {data.topics.map((topic) => (
             <TopicItem topic={topic} />
