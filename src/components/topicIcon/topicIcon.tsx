d5e73b752d2698101ea9c965c95d701713122171
Sandravaphilips <lacasandra1@gmail.com>
2021-03-31 03:08:32 -0700

    built the topic icon component

---
+++ src/components/topicIcon/topicIcon.tsx
@@ -0,0 +1,29 @@
+import { IonCard, IonIcon } from "@ionic/react";
+import React from "react";
+import "./topicIcon.css";
+
+interface TopicIconProps {
+  topic: any;
+}
+
+export const TopicIcon: React.FC<TopicIconProps> = ({ topic }) => {
+  const formatTitle = (str: string) => {
+    return str.substring(str.indexOf(":") + 1);
+  };
+  return (
+    <IonCard class="topic-icon ion-no-margin">
+      <div>
+        <span className="guide">A Guide to</span>
+        <div className="topic_title">{formatTitle(topic.title)}</div>
+        <IonIcon
+          size="large"
+          class="main_icon"
+          icon={"../../assets/images/" + topic.id + ".svg"}
+          onError={(e) => {
+            e.currentTarget.src = "../../assets/shapes.svg";
+          }}
+        ></IonIcon>
+      </div>
+    </IonCard>
+  );
+};
