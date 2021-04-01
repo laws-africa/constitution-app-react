24e3e1017f3c2dfd667d086993a55049def0051b
Sandravaphilips <lacasandra1@gmail.com>
2021-04-01 06:33:21 -0700

    moved the flattenTOC function to utils

--- src/data/constitution.tsx
+++ src/data/constitution.tsx
@@ -1,4 +1,5 @@
 import * as Constitution from '../assets/data/constitution.json';
+import { flattenTOC } from "../utils";
 
 export const constitutionData: any = (Constitution as any).default;
 // wrap the entire content in a div so the body has a single child
@@ -33,19 +34,6 @@ export function getTOCEntry(id: string) {
 
     search(tableOfContents);
     return match;
-}
-
-function flattenTOC (arr: any[]) {
-    let result: string[] = [];
-
-    arr.forEach(el => {
-        if (el.id) result.push(el.id);
-        if (el.children && el.children.length > 0) {
-            result = result.concat(flattenTOC(el.children));
-        };
-    });
-
-    return result;
 }
 
 export const flatTOC = flattenTOC(tableOfContents);
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,16 @@
 export const handleSupportersLink = (url: string) => {
   window.open(url, '_blank');
 };
+
+export function flattenTOC (arr: any[]) {
+    let result: string[] = [];
+
+    arr.forEach(el => {
+        if (el.id) result.push(el.id);
+        if (el.children && el.children.length > 0) {
+            result = result.concat(flattenTOC(el.children));
+        };
+    });
+
+    return result;
+}
