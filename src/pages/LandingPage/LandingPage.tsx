b31e26d6fcd1928c9b509615d21339fd3a0d8e74
Sandravaphilips <lacasandra1@gmail.com>
2021-05-28 02:32:53 -0700

    Built the Landing page

---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,246 @@
+import {
+  IonCol,
+  IonRow,
+  IonIcon,
+  IonPage,
+  IonButton,
+  IonContent,
+  IonSegment,
+  IonSegmentButton,
+  IonFooter,
+  IonGrid,
+} from "@ionic/react";
+import React from "react";
+import { svgs } from "../../assets/svgs";
+import { handleSupportersLink } from "../../utils";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <IonPage>
+      <IonContent>
+        <IonRow class="ion-justify-content-between ion-margin-horizontal ion-padding-vertical header ion-align-items-center">
+          <section className="tab-title">
+            <IonCol size="2" class="home-icon ion-no-padding">
+              <IonIcon
+                icon={
+                  "../../assets/images/sec_2__Supremacy_of_the_Constitution.svg"
+                }
+                onError={(e) => {
+                  e.currentTarget.src = "../../assets/shapes.svg";
+                }}
+              ></IonIcon>
+            </IonCol>
+            <h3>
+              Constitution <br /> Guide
+            </h3>
+          </section>
+          <IonRow class="ion-align-items-center right-links ion-justify-content-between">
+            <a href="#features">Features</a>{" "}
+            <a href="#installation">How to install</a>
+            <IonButton color="light">Install Now</IonButton>
+          </IonRow>
+          <IonCol size="2" class="menu-icon ion-no-margin ion-no-padding">
+            <IonIcon
+              icon={"../../assets/menu-icon.svg"}
+              onError={(e) => {
+                e.currentTarget.src = "../../assets/shapes.svg";
+              }}
+              class="ion-no-padding ion-no-margin"
+            ></IonIcon>
+          </IonCol>
+        </IonRow>
+        <div className="ion-text-center main-content">
+          <h1>Short version of the purpose of this app. </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <IonButton color="light">Install Now</IonButton>
+        </div>
+        <div id="features" className="features-content">
+          <h2>Introducing the features of this product</h2>
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <Features
+                  iconName={svgs.CONSTITUTION}
+                  text="Filium morte multavit si sine causa quae fuerit causa mox videro interea epicurus in sanguinem suum tam egregios viros consectetur"
+                  title="Browse the Constitution"
+                />
+              </IonCol>
+              <IonCol>
+                <Features
+                  iconName={svgs.GUIDES}
+                  text="Filium morte multavit si sine causa quae fuerit causa mox videro interea epicurus in sanguinem suum tam egregios viros consectetur"
+                  title="Guides to the Constitution"
+                />
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <Features
+                  iconName={svgs.CASES}
+                  text="Filium morte multavit si sine causa quae fuerit causa mox videro interea epicurus in sanguinem suum tam egregios viros consectetur"
+                  title="Get Cases in Context "
+                />
+              </IonCol>
+              <IonCol>
+                <Features
+                  iconName={svgs.RULES}
+                  text="Filium morte multavit si sine causa quae fuerit causa mox videro interea epicurus in sanguinem suum tam egregios viros consectetur"
+                  title="Rules of the National Assembly"
+                />
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <Features
+                  iconName={svgs.SEARCH}
+                  text="Filium morte multavit si sine causa quae fuerit causa mox videro interea epicurus in sanguinem suum tam egregios viros consectetur"
+                  title="Find what you’re looking for"
+                />
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </div>
+        <div id="installation" className="installation">
+          <h2>How to install the app on your phone</h2>
+          <IonSegment mode="md" value="ios">
+            <IonSegmentButton value="ios">iOS</IonSegmentButton>
+            <IonSegmentButton value="android">Android</IonSegmentButton>
+          </IonSegment>
+          <hr />
+          <IonRow class="ion-justify-content-between">
+            <figure></figure>
+            <div className="installation-steps">
+              <h6>Add to your iPhone</h6>
+              <ol>
+                <li>
+                  Open your Safari browser{" "}
+                  <span>
+                    Note: It must be Safari, you cannot use Google Chrome or
+                    Firefox.
+                  </span>
+                </li>
+                <li>Go to bit.ly/saconapp or constitution.laws.africa</li>
+                <li>Tap the share icon at the bottom of the screen</li>
+                <li>Scroll down and tap “Add to Home Screen” then tap “Add”</li>
+                <li>To open the app, find it on your home screen </li>
+              </ol>
+            </div>
+          </IonRow>
+        </div>
+        <IonFooter>
+          <IonRow class="ion-justify-content-between ion-padding-vertical header ion-align-items-center">
+            <section className="tab-title">
+              <IonCol size="2" class="home-icon ion-no-padding">
+                <IonIcon
+                  icon={
+                    "../../assets/images/sec_2__Supremacy_of_the_Constitution.svg"
+                  }
+                  onError={(e) => {
+                    e.currentTarget.src = "../../assets/shapes.svg";
+                  }}
+                ></IonIcon>
+              </IonCol>
+              <h3>
+                Constitution <br /> Guide
+              </h3>
+            </section>
+            <IonCol class="footer-links">
+              <a href="#features">Features</a>{" "}
+              <a href="#installation">How to install</a>
+              <span>Install Now</span>
+            </IonCol>
+          </IonRow>
+          <IonRow class="ion-justify-content-between ion-padding-vertical last-content ion-align-items-center">
+            <span>&copy; 2021 UCT Legal Division.</span>
+            <IonGrid class="ion-align-items-center">
+              <span>Developed By:</span>
+              <IonRow>
+                <IonCol size="6">
+                  <img
+                    alt=""
+                    src="../../assets/African Lii logo.svg"
+                    onError={(e) => {
+                      e.currentTarget.src = "../../assets/shapes.svg";
+                    }}
+                    onClick={() =>
+                      handleSupportersLink("https://africanlii.org/")
+                    }
+                  />
+                </IonCol>
+                <IonCol size="6">
+                  <img
+                    alt=""
+                    src="../../assets/03_KAS_Logo_Min_RGB_Blau.jpg"
+                    onError={(e) => {
+                      e.currentTarget.src = "../../assets/shapes.svg";
+                    }}
+                    onClick={() =>
+                      handleSupportersLink("https://www.kas.de/en/home")
+                    }
+                  />
+                </IonCol>
+                <IonCol size="6">
+                  <img
+                    alt=""
+                    src="../../assets/uc-tlogo@2x.jpg"
+                    onError={(e) => {
+                      e.currentTarget.src = "../../assets/shapes.svg";
+                    }}
+                    onClick={() =>
+                      handleSupportersLink("https://www.uct.ac.za/")
+                    }
+                  />
+                </IonCol>
+                <IonCol size="6">
+                  <img
+                    alt=""
+                    src="../../assets/Laws.Africa.jpg"
+                    onError={(e) => {
+                      e.currentTarget.src = "../../assets/shapes.svg";
+                    }}
+                    onClick={() =>
+                      handleSupportersLink("https://www.uct.ac.za/")
+                    }
+                  />
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonRow>
+        </IonFooter>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default LandingPage;
+
+interface FeaturesProps {
+  iconName: string;
+  title: string;
+  text: string;
+}
+
+const Features: React.FC<FeaturesProps> = ({ iconName, title, text }) => {
+  return (
+    <div className="feature-row">
+      <IonCol class="feature_col">
+        <IonIcon
+          size="large"
+          class="feature_icon"
+          icon={iconName}
+          onError={(e) => {
+            e.currentTarget.src = "../../assets/shapes.svg";
+          }}
+        ></IonIcon>
+      </IonCol>
+      <section className="feature-details">
+        <h6>{title}</h6>
+        <p>{text}</p>
+      </section>
+    </div>
+  );
+};
