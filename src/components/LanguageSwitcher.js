2b8b125d36b32ef1f1e9be2e6a3f4d4f5c68aa4c
Musango <musangow@gmail.com>
2021-12-01 09:10:57 +0200

    create Test language switcher component

---
+++ src/components/LanguageSwitcher.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+function LanguageSwitcher() {
+  const { i18n } = useTranslation();
+  return (
+      <div className="select">
+        <select
+            value={i18n.language}
+            onChange={(e) =>
+                i18n.changeLanguage(e.target.value)
+            }
+        >
+          {i18n.options.supportedLngs.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+          ))}
+        </select>
+      </div>
+  );
+}
+export default LanguageSwitcher;
