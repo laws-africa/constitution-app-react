fd43b1a707955c70987dadbd732600cf8a9ec6b0
Musango <musangow@gmail.com>
2021-12-06 17:25:30 +0200

    Help.tsx: wrap text in translate functions for localizing

--- src/pages/help/Help.tsx
+++ src/pages/help/Help.tsx
@@ -3,8 +3,11 @@ import { IonCol, IonContent, IonIcon, IonPage } from "@ionic/react";
 import "./Help.css";
 import { svgs } from "../../assets/svgs";
 import ActionAnchorLink from "../../components/Action/ActionAnchorLink";
+import {useTranslation} from "react-i18next";
+import parse from "html-react-parser";
 
 const Tab1: React.FC = () => {
+  const { t } = useTranslation('help');
   return (
     <IonPage>
       <IonContent className="ion-padding">
@@ -12,28 +15,29 @@ const Tab1: React.FC = () => {
           <IonCol size="1" class="icon ion-no-padding">
             <IonIcon size="small" icon={svgs.HELP}></IonIcon>
           </IonCol>
-          <h2>About</h2>
+          <h2>{t('about_heading', "About")}</h2>
         </section>
 
         <hr className="header-divider" />
-        <p>
-          The Constitution App is developed by the{" "}
+        {parse(t('app_description', `
+          <p>
+            The Constitution App is developed by the&nbsp;
           <a
             href="http://www.dgru.uct.ac.za/"
             target="_blank"
             rel="noopener noreferrer"
           >
             Democratic Governance and Rights Unit
-          </a>{" "}
-          of the Faculty of Law, University of Cape Town in collaboration with{" "}
+          </a>&nbsp;
+          of the Faculty of Law, University of Cape Town in collaboration with&nbsp;
           <a
             href="https://africanlii.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             AfricanLII
-          </a>{" "}
-          and{" "}
+          </a>&nbsp;
+          and&nbsp;
           <a
             href="https://laws.africa"
             target="_blank"
@@ -41,7 +45,7 @@ const Tab1: React.FC = () => {
           >
             Laws.Africa
           </a>
-          , with funding support from the{" "}
+          , with funding support from the&nbsp;
           <a
             href="https://www.kas.de/en/web/suedafrika/home"
             target="_blank"
@@ -53,7 +57,7 @@ const Tab1: React.FC = () => {
           with accurate and authoritative information on South African
           constitutional law.
         </p>
-        <p>
+          <p>
           In this application, you will find an updated version of the South
           African constitution, together with commentary of its key provisions
           and their interpretation by South Africa’s highest courts. The app
@@ -61,31 +65,35 @@ const Tab1: React.FC = () => {
           text of constitutional court judgments. The content of this
           application is available off-line and will be updated twice a year.
         </p>
-        <h3>Contact</h3>
-        <p>
-          Feel free to forward constructive feedback and suggestions to{" "}
-          <a href="mailto:info@africanlii.org">info@africanlii.org</a>.
-        </p>
+        `))}
+        {parse(t('contact_us', `
+          <h3>Contact</h3>
+          <p>
+            Feel free to forward constructive feedback and suggestions to&nbsp;
+            <a href="mailto:info@africanlii.org">info@africanlii.org</a>.
+          </p>
+        `))}
 
-        <h3>Copyright</h3>
-        <p>
-          The contents of this app is licensed under Attribution-NonCommercial
-          4.0 International (CC BY-NC 4.0) Creative Commons license. View the
-          license at{" "}
-          <a
-            href="https://creativecommons.org/licenses/by-nc/4.0/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            https://creativecommons.org/licenses/by-nc/4.0/
-          </a>
-          .
-        </p>
-
+        {parse(t('copyright', `
+          <h3>Copyright</h3>
+          <p>
+            The contents of this app is licensed under Attribution-NonCommercial
+            4.0 International (CC BY-NC 4.0) Creative Commons license. View the
+            license at&nbsp;
+            <a
+              href="https://creativecommons.org/licenses/by-nc/4.0/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://creativecommons.org/licenses/by-nc/4.0/
+            </a>
+            .
+          </p>
+        `))}
         <div>
           <ActionAnchorLink
             href="mailto:info@africanlii.org"
-            actionText="Send feedback"
+            actionText={parse(t('send_feedback_button_title', 'Send feedback'))}
           />
         </div>
       </IonContent>
