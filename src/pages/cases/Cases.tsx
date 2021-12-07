afa14a7f05b4b943ff6cdc31017620d16cd652db
Musango <musangow@gmail.com>
2021-12-07 14:57:07 +0200

    localize Case pages with t function

--- src/pages/cases/Cases.tsx
+++ src/pages/cases/Cases.tsx
@@ -11,21 +11,23 @@ import {
 import data from "../../assets/data/data.json";
 import './Cases.css';
 import { CaseItem } from "../../components/case";
+import {useTranslation} from "react-i18next";
 
 const Cases: React.FC = () => {
   const cases = data.cases;
+  const { t } = useTranslation('case');
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Cases</IonTitle>
+          <IonTitle>{t('page_title', 'Cases')}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonText>
           <div className="ion-padding">
-            Summaries of cases from the Constitutional Court.
+            {t('description_text', 'Summaries of cases from the Constitutional Court.')}
           </div>
         </IonText>
         <IonList>
