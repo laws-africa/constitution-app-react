0dad61cb049b1e58cd01f3e6aac39bbefeded23a
Musango <musangow@gmail.com>
2021-12-07 15:00:38 +0200

    localize topic related pages

--- src/pages/topics/Topics.tsx
+++ src/pages/topics/Topics.tsx
@@ -11,8 +11,10 @@ import data from "../../assets/data/data.json";
 import "./Topics.css";
 import { TopicItem } from "../../components/topic";
 import { svgs } from "../../assets/svgs";
+import {useTranslation} from "react-i18next";
 
 const Topics: React.FC = () => {
+  const { t } = useTranslation('topic');
   return (
     <IonPage>
       <IonContent>
@@ -21,19 +23,19 @@ const Topics: React.FC = () => {
             <IonCol size="1" class="icon ion-no-padding">
               <IonIcon size="small" icon={svgs.GUIDES}></IonIcon>
             </IonCol>
-            <h2>Guides</h2>
+            <h2>{t('page_title', 'Guides')} </h2>
             <IonButton
               className="ion-no-padding"
               fill="clear"
               routerLink={"/search/guides"}
             >
-              Search
+              {t('search_button_label', 'Search')}
             </IonButton>
           </section>
 
           <hr className="header-divider" />
 
-          <p>Guides to the sections of the Constitution.</p>
+          <p>{t('section_description_text', 'Guides to the sections of the Constitution.')}</p>
           <hr className="list-divider" />
         </div>
         <IonList className="ion-padding">
