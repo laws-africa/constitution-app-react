cd4b494bc524c9f0d3981e6f32d0abac9d234224
Musango <musangow@gmail.com>
2022-01-03 09:15:23 +0200

    constitutionTOC.tsx: apply virtualized list with flat provisions

--- src/components/constitutionTOC.tsx
+++ src/components/constitutionTOC.tsx
@@ -1,24 +1,73 @@
-import React from "react";
+import React, {useRef} from "react";
 import { IonItem } from "@ionic/react";
+import { VariableSizeList as List } from "react-window";
 import { toc } from "../data/constitution";
+import {useWindowSize} from "../custom-hooks/useWindowResize";
 
 interface TOCListProps {
   onClick?: any;
 }
 
-// only render provisions with ids
-const provisions = toc.items.filter((c: any) => c.id);
+// @ts-ignore
+const Row = ({ index, setSize, rowData, windowWidth, onClick } ) => {
+  const rowRef = React.useRef<HTMLElement | null>(null);
+  React.useEffect(() => {
+    if(rowRef && rowRef.current) {
+      // @ts-ignore
+      setSize(String(index), rowRef.current.getBoundingClientRect().height);
+    }
+  }, [setSize, index, windowWidth]);
+  return (
+      <div
+          // @ts-ignore
+          ref={rowRef}
+      >
+        <TOCItem
+            item={rowData}
+            onClick={onClick}
+        />
+      </div>
+  )
+};
 
 export const TOCList: React.FC<TOCListProps> = ({ onClick }) => {
+  const listRef = React.useRef(null);
+  const sizeMap = React.useRef({});
+  const setSize = React.useCallback((index, size) => {
+    sizeMap.current = { ...sizeMap.current, [index]: size };
+    // @ts-ignore
+    listRef.current.resetAfterIndex(index);
+  }, []);
+  // @ts-ignore
+  const getSize = index => sizeMap.current[index] || 50;
+  const [windowWidth] = useWindowSize();
+
+  // only render provisions with ids
+  const provisions = React.useMemo(
+      () => toc.flattenedDeep.filter((c: any) => c.id),
+      []
+  );
   return (
       <div>
-        {provisions.map((item: any) => {
-          return (
-              <div key={item.id}>
-                <TOCItem item={item} onClick={onClick}/>
+        <List
+            ref={listRef}
+            className="List"
+            height={400}
+            itemCount={provisions.length}
+            itemSize={getSize}
+            width="100%"
+        >
+          {({ index, style }) =>
+              <div style={style} key={index}>
+                <Row windowWidth={windowWidth}
+                     onClick={onClick}
+                     setSize={setSize}
+                     index={index}
+                     rowData={provisions[index]}
+                />
               </div>
-          );
-        })}
+          }
+        </List>
       </div>
   );
 };
@@ -28,8 +77,7 @@ interface TOCItemProps {
   onClick?: any;
 }
 
-export const TOCItem: React.FC<TOCItemProps> = ({ item, onClick }) => {
-  const kids = (item.children || []).filter((c: any) => c.id);
+const TOCItem: React.FC<TOCItemProps> = ({ item, onClick }) => {
   const props = onClick ? {onClick: () => onClick(item)} : {routerLink: '/constitution/provision/' + item.id};
 
   return (
@@ -37,13 +85,6 @@ export const TOCItem: React.FC<TOCItemProps> = ({ item, onClick }) => {
       <IonItem class={item.type === "chapter" ? "chapter" : ""} {...props}>
         {item.title}
       </IonItem>
-      {kids.length > 0 && (
-        <div className="ion-padding-start">
-          {kids.map((child: any) => {
-            return <TOCItem key={child.id} item={child} onClick={onClick} />;
-          })}
-        </div>
-      )}
     </div>
   );
 };
