e95987ed5dff2be166c8594b50dfc96a41120e37
Musango <musangow@gmail.com>
2022-01-03 09:13:30 +0200

    rulesTOC.tsx: change maps of provisions to sample virtual list
    for performance testing on mobile

--- src/components/rulesTOC.tsx
+++ src/components/rulesTOC.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { IonItem } from "@ionic/react";
 import { toc } from "../data/rules";
+import { FixedSizeList as List } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
 
 interface TOCListProps {
   onClick?: any;
@@ -9,16 +11,32 @@ interface TOCListProps {
 // only render provisions with ids
 const provisions = toc.items.filter((c: any) => c.id);
 
+// @ts-ignore
+const Row = ({ index, style }) => (
+    <div style={style}>
+      Row {index}
+    </div>
+);
+
 export const TOCList: React.FC<TOCListProps> = ({ onClick }) => {
   return (
       <div>
-        {provisions.map((item: any) => {
-          return (
-              <div key={item.id}>
-                <RuleTOCItem item={item} onClick={onClick}/>
-              </div>
-          );
-        })}
+        <List
+            className="List"
+            height={700}
+            itemCount={1000}
+            itemSize={35}
+            width={300}
+        >
+          {Row}
+        </List>
+        {/*{provisions.map((item: any) => {*/}
+        {/*  return (*/}
+        {/*      <div key={item.id}>*/}
+        {/*        <RuleTOCItem item={item} onClick={onClick}/>*/}
+        {/*      </div>*/}
+        {/*  );*/}
+        {/*})}*/}
       </div>
   );
 };
