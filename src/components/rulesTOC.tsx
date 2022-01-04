feb5feb51936387eccaa9ecb9f1b0adf0a804909
Musango <musangow@gmail.com>
2022-01-04 19:58:14 +0200

    rulesTOC.tsx: remove experimental  Autosizer and react window logic

--- src/components/rulesTOC.tsx
+++ src/components/rulesTOC.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { IonItem } from "@ionic/react";
 import { toc } from "../data/rules";
-import { FixedSizeList as List } from "react-window";
-import AutoSizer from "react-virtualized-auto-sizer";
 
 interface TOCListProps {
   onClick?: any;
@@ -11,32 +9,16 @@ interface TOCListProps {
 // only render provisions with ids
 const provisions = toc.items.filter((c: any) => c.id);
 
-// @ts-ignore
-const Row = ({ index, style }) => (
-    <div style={style}>
-      Row {index}
-    </div>
-);
-
 export const TOCList: React.FC<TOCListProps> = ({ onClick }) => {
   return (
       <div>
-        <List
-            className="List"
-            height={700}
-            itemCount={1000}
-            itemSize={35}
-            width={300}
-        >
-          {Row}
-        </List>
-        {/*{provisions.map((item: any) => {*/}
-        {/*  return (*/}
-        {/*      <div key={item.id}>*/}
-        {/*        <RuleTOCItem item={item} onClick={onClick}/>*/}
-        {/*      </div>*/}
-        {/*  );*/}
-        {/*})}*/}
+        {provisions.map((item: any) => {
+          return (
+              <div key={item.id}>
+                <RuleTOCItem item={item} onClick={onClick}/>
+              </div>
+          );
+        })}
       </div>
   );
 };
