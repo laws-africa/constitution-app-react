d6faf49c5d73e935af814da619c2f1c433c25158
Musango <musangow@gmail.com>
2022-01-18 17:00:12 +0200

    LanguageSwitcher.js: change to ion components for aestetics

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -1,24 +1,35 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import {IonSelect, IonSelectOption} from "@ionic/react";
+import {svgs} from "../assets/svgs";
 
 function LanguageSwitcher() {
   const { i18n } = useTranslation();
+  const supportedLngs = i18n.options.supportedLngs.filter(option => option !==  'cimode');
   return (
-      <div>
-        <div className="select">
-          <select
-              value={i18n.language}
-              onChange={(e) =>
-                  i18n.changeLanguage(e.target.value)
-              }
-          >
-            {i18n.options.supportedLngs.map((option, index) => (
-                <option key={index} value={option}>
-                  {option}
-                </option>
-            ))}
-          </select>
-        </div>
+      <div
+          className="language-switcher">
+        <img src={svgs.LANGUAGE} alt="language-switcher" style={{
+          width: "28px",
+          height: "28px"
+        }}/>
+        <IonSelect
+            interface="popover"
+            mode="ios"
+            style={{
+              paddingStart: "10px"
+            }}
+            value={i18n.language}
+            onIonChange={(e) =>
+                i18n.changeLanguage(e.target.value)
+            }
+        >
+          {supportedLngs.map((option, index) => (
+              <IonSelectOption key={index} value={option}>
+                {option}
+              </IonSelectOption>
+          ))}
+        </IonSelect>
       </div>
   );
 }
