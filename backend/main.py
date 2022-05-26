77c685b79f7b6c42274a0b5fc72c39a5d1ecf80f
Greg Kempe <greg@kempe.net>
2022-05-26 16:15:29 +0200

    Map three letter lang codes to 2

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,12 @@ PRODUCTION_DOCUMENTS_OUTPUT_PATH = r'../src/assets/data/'
 # Get your auth token from https://edit.laws.africa/accounts/profile/api/
 INDIGO_AUTH_TOKEN = os.environ.get('INDIGO_AUTH_TOKEN')
 
+LANG_3_TO_2 = {
+    'eng': 'en',
+    'xho': 'xh',
+    'zul': 'zu',
+}
+
 
 def write_work(name, frbr_uri):
     headers = {'Authorization': f'Token {INDIGO_AUTH_TOKEN}'}
@@ -27,18 +33,19 @@ def write_work(name, frbr_uri):
     date = data['expression_date']
 
     # first language
-    languages = {data['language']: data}
+    languages = {LANG_3_TO_2[data['language']]: data}
 
     # all other languages at this date
     others = data['points_in_time'][-1]
     assert others['date'] == date
     for expr in others['expressions']:
-        if expr['language'] not in languages:
+        lang = LANG_3_TO_2[expr['language']]
+        if lang not in languages:
             frbr_uri = expr['expression_frbr_uri']
             print(f"Getting {frbr_uri}")
             resp = requests.get(f'https://api.laws.africa/v2{frbr_uri}/.json', headers=headers)
             resp.raise_for_status()
-            languages[expr['language']] = resp.json()
+            languages[lang] = resp.json()
 
     # fetch all the details for each language
     for lang in languages.values():
@@ -79,10 +86,11 @@ def clean_toc(toc):
 
 
 def main():
-    cases = read_google_sheets('cases')
-    topics = read_google_sheets('topics')
-    write_all_documents(cases, topics)
-    # write_work("constitution", "/akn/za/act/1996/constitution")
+    # cases = read_google_sheets('cases')
+    # topics = read_google_sheets('topics')
+    # write_all_documents(cases, topics)
+    write_work("constitution", "/akn/za/act/1996/constitution")
+    # TODO: NB NB NB if this is enabled, the rules-reading code in the app must be able to handle multiple languages
     # write_work("rules", "/akn/za/act/rules/2016/national-assembly/")
 
 
