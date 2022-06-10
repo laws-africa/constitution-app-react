bf7635c38cff988196e90dfc864cc507bbfd1590
Musango <musangow@gmail.com>
2022-06-10 18:05:26 +0200

    Topics.tsx:  refactor guides with useLanguage

--- src/pages/topics/Topics.tsx
+++ src/pages/topics/Topics.tsx
@@ -12,48 +12,52 @@ import { TopicItem } from "../../components/topic";
 import { svgs } from "../../assets/svgs";
 import {useTranslation} from "react-i18next";
 
-import {Virtuoso} from "react-virtuoso";
+import { Virtuoso } from "react-virtuoso";
 import {useWindowSize} from "../../custom-hooks/useWindowResize";
-import { guides } from "../../data/guides";
+import { getGuides, Guide} from "../../data/guides";
+import {useLanguage} from "../../custom-hooks/useLanguage";
 
 const Topics: React.FC = () => {
   const [windowWidth] = useWindowSize();
-  const { t } = useTranslation('topic');
+  const lang = useLanguage();
+
+  const {t} = useTranslation('topic');
+  const guides = getGuides(lang)
   return (
-    <IonPage>
-      <IonContent className="virtual-list-container">
-        <div className="ion-padding">
-          <section className="tab-title">
-            <IonCol size="1" class="icon ion-no-padding">
-              <IonIcon size="small" icon={svgs.GUIDES}></IonIcon>
-            </IonCol>
-            <h2>{t('page_title', 'Guides')} </h2>
-            <IonButton
-              className="ion-no-padding"
-              fill="clear"
-              routerLink={"/search/guides"}
-            >
-              {t('search_button_label', 'Search')}
-            </IonButton>
-          </section>
+      <IonPage>
+        <IonContent className="virtual-list-container">
+          <div className="ion-padding">
+            <section className="tab-title">
+              <IonCol size="1" class="icon ion-no-padding">
+                <IonIcon size="small" icon={svgs.GUIDES}></IonIcon>
+              </IonCol>
+              <h2>{t('page_title', 'Guides')} </h2>
+              <IonButton
+                  className="ion-no-padding"
+                  fill="clear"
+                  routerLink={"/search/guides"}
+              >
+                {t('search_button_label', 'Search')}
+              </IonButton>
+            </section>
 
-          <hr className="header-divider" />
+            <hr className="header-divider"/>
 
-          <p>{t('section_description_text', 'Guides to the sections of the Constitution.')}</p>
-          <hr className="list-divider" />
-        </div>
-        <IonList className="ion-padding">
-          {windowWidth <= 425 ? (
-              <Virtuoso
-                  style={{ height: '100%' }}
-                  totalCount={guides.length}
-                  itemContent={(index) => {
-                    return (
-                        <TopicItem topic={guides[index]} key={guides[index].id} />
-                    );
-                  }}
-              />
-          ) : guides.map(item => <TopicItem topic={item} key={item.id} /> )}
+            <p>{t('section_description_text', 'Guides to the sections of the Constitution.')}</p>
+            <hr className="list-divider"/>
+          </div>
+          <IonList className="ion-padding">
+            {windowWidth <= 425 ? (
+                <Virtuoso
+                    style={{height: '100%'}}
+                    totalCount={guides.length}
+                    itemContent={(index: string | number) => {
+                      return (
+                          <TopicItem topic={guides[index]} key={guides[index].id}/>
+                      );
+                    }}
+                />
+            ) : guides.map((item: Guide) => <TopicItem topic={item} key={item.id} /> )}
         </IonList>
       </IonContent>
     </IonPage>
